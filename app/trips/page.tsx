6607bb4b6ed26b4707bd605c86004916dc527074
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-05-30 21:01:55 +0200

    Update page.tsx

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -3,7 +3,7 @@ import EmptyState from "@/app/components/EmptyState";
 import ClientOnly from "@/app/components/ClientOnly";
 
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import getReservations from "@/app/actions/getReservations";
+import getReservations from "@/app/actions/getReservation";
 
 import TripsClient from "./TripsClient";
 
