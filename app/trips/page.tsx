17f80af08282f2c30c0864aa493d2ae8e65af8d7
Davide Filosa <52102667+davidefilosa@users.noreply.github.com>
2023-05-30 22:04:51 +0200

    Update page.tsx

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,46 +1,37 @@
-
-import EmptyState from "@/app/components/EmptyState";
-import ClientOnly from "@/app/components/ClientOnly";
-
-import getCurrentUser from "../actions/getCurrentUser";
-import getReservations from "../actions/getReservation
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import getCurrenUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservation";
 import TripsClient from "./TripsClient";
 
 const TripsPage = async () => {
-  const currentUser = await getCurrentUser();
+  const currentUser = await getCurrenUser();
+  console.log(currentUser);
 
   if (!currentUser) {
     return (
       <ClientOnly>
-        <EmptyState
-          title="Unauthorized"
-          subtitle="Please login"
-        />
+        <EmptyState title="Unauthorized" subtitle="Please login" />
       </ClientOnly>
     );
   }
-
   const reservations = await getReservations({ userId: currentUser.id });
 
-  if (reservations.length === 0) {
+  if (reservations?.length === 0) {
     return (
       <ClientOnly>
         <EmptyState
-          title="No trips found"
-          subtitle="Looks like you havent reserved any trips."
+          title="There are no trips found"
+          subtitle="Looks like you havent reserved any trips"
         />
       </ClientOnly>
     );
   }
-
   return (
     <ClientOnly>
-      <TripsClient
-        reservations={reservations}
-        currentUser={currentUser}
-      />
+      <TripsClient reservations={reservations} currentUser={currentUser} />
     </ClientOnly>
   );
-}
- 
+};
+
 export default TripsPage;
